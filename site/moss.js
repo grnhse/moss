b69eb21dfbf61ba58a07062443bddcd069e0e215
Allen Krulwich <akrulwich@greenhouse.io>
2016-06-20 12:21:57 -0400

    Finish AST

--- site/moss.js
+++ site/moss.js
@@ -1,71 +1,79 @@
 function Moss(dataString) {
   var icBlockNodes = {};
   var ics = {};
-  dataString.split(/\n\n+/).forEach(function(block) {
-    icBlockNodes[icOf(block)] = BlockNode(block);
+  dataString.split(/\n\n+(?=.)/).forEach(function(block) {
     ics[icOf(block)] = icOf(block);
   });
 
-  var ast = assembleTree(icBlockNodes[icOf(dataString)], icBlockNodes)
+  dataString.split(/\n\n+(?=.)/).forEach(function(block) {
+    icBlockNodes[icOf(block)] = new BlockNode(block);
+  });
+
+  var ast = assembleTree(icBlockNodes[icOf(dataString)], icBlockNodes);
   return ast;
-}
 
-function BlockNode(block) {
-  this.lines = block.split('\n').map(LineNode);
-  this.children = [];
-}
-
-function LineNode(line) {
-  this.tokens = [];
-  line.match(/ ?([^,.:;?!)'"]+[,.:;?!)'"]+)/).forEach(clauseWithPunctuation) {
-    var puctuation = clauseWithPuctuation.match(/[,.:;?!)'"]+/);
-    var clause = clauseWithPunctuation.slice(0, -punctuation.length);
-    var words = clause.split(' ');
-    while (words.length > 0) {
-      next_prefix:
-      for (var i = words.length; i > 0; i--) {
-        var substring = words.slice(0, i).join(' ');
-        if (ics.hasOwnProperty(substring.toLowerCase())) {
-          this.tokens.push(LinkToken(substring));
-          words = words.slice(i);
-          break next_prefix;
-        }
-        var word = words.shift();
-        tokens.push(TextToken(word));
-      }
-    }
+  function BlockNode(block) {
+    this.text = block;
+    this.lines = block.trim().split('\n').map(function(block) { return new LineNode(block);});
+    this.children = [];
   }
-}
 
-function assembleTree(parentNode, icBlockNodes) {
-  parentNode.lines.forEach(lineNode) {
-    lineNode.tokens.filter(function(token){token.constructor === LinkToken}).forEach(function(linkToken, index) {
-      if (index === 0) {
-        linkToken.type = 'ic';
-      } else if (icBlockNodes.hasOwnProperty(linkToken.text.toLowerCase())) {
-        parentNode.children.push(icBlockNodes[]);
-        delete icBlockNodes[linkToken.ic];
-        linkToken.type = 'primary'
-      } else {
-        linkToken.type = 'secondary'
+  function LineNode(line) {
+    this.text = line;
+    var lineNode = this;
+    lineNode.tokens = [];
+    line.match(/ ?([^,.:;?!)'"]+[,.:;?!)'"]+)/g).forEach(function(clauseWithPunctuation) {
+      var punctuation = clauseWithPunctuation.match(/[,.:;?!)'"]+/);
+      var clause = clauseWithPunctuation.slice(0, -punctuation.length);
+      var words = clause.split(' ');
+      while (words.length > 0) {
+        next_while_loop:
+        for (var i = words.length; i > 0; i--) {
+          var substring = words.slice(0, i).join(' ');
+          if (ics.hasOwnProperty(substring.toLowerCase())) {
+            lineNode.tokens.push(new LinkToken(substring));
+            words = words.slice(i);
+            break next_while_loop;
+          }
+          var word = words.shift();
+          lineNode.tokens.push(new TextToken(word));
+        }
       }
     });
   }
 
-  parentNode.children.forEach(function(childNode) {
-    assembleTree(childNode, icBlockNodes);
-  });
-}
+  function assembleTree(parentNode, icBlockNodes) {
+    parentNode.lines.forEach(function(lineNode, lineIndex) {
+      lineNode.tokens.filter(function(token){return token.constructor === LinkToken}).forEach(function(linkToken, tokenIndex) {
+        if (lineIndex === 0 && tokenIndex === 0) {
+          linkToken.type = 'ic';
+        } else if (icBlockNodes.hasOwnProperty(linkToken.ic)) {
+          parentNode.children.push(icBlockNodes[linkToken.ic]);
+          delete icBlockNodes[linkToken.ic];
+          linkToken.type = 'primary'
+        } else {
+          linkToken.type = 'secondary'
+        }
+      });
+    });
 
-function TextToken(text) {
-  this.text = text;
-}
+    parentNode.children.forEach(function(childNode) {
+      assembleTree(childNode, icBlockNodes);
+    });
 
-function LinkToken(text) {
-  this.text = text;
-  this.ic = text.toLowerCase();
-}
+    return parentNode;
+  }
 
-function icOf(string) {
-  block.split(/[,.;:]/)[0].toLowerCase();
+  function TextToken(text) {
+    this.text = text;
+  }
+
+  function LinkToken(text) {
+    this.text = text;
+    this.ic = text.toLowerCase();
+  }
+
+  function icOf(string) {
+    return string.split(/[,.;:?!]/)[0].toLowerCase();
+  }
 }
