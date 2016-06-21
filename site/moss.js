031d7bb0d4c7f2479886f0c27e6ae7ef4cbdccd0
Allen Krulwich <akrulwich@greenhouse.io>
2016-06-21 12:26:54 -0400

    Add test and make constructors testable

--- site/moss.js
+++ site/moss.js
@@ -10,85 +10,85 @@ function Moss(dataString) {
 
   // Create flat set of block nodes
   dataString.split(/\n\n+(?=.)/).forEach(function(block) {
-    icBlockNodes[icOf(block)] = new BlockNode(block);
+    icBlockNodes[icOf(block)] = new BlockNode(block, ics);
   });
 
   // Assemble block nodes into tree
-  var ast = assembleTree(icBlockNodes[icOf(dataString)], icBlockNodes);
+  var ast = assembleTree(icBlockNodes[icOf(dataString)], icBlockNodes, ics);
   return ast;
+}
 
-  function BlockNode(block) {
-    this.text = block;
-    this.lines = block.trim().split('\n').map(function(block) { return new LineNode(block);});
-    this.children = [];
-  }
+function BlockNode(block, ics) {
+  this.text = block;
+  this.lines = block.trim().split('\n').map(function(block) { return new LineNode(block, ics);});
+  this.children = [];
+}
 
-  function LineNode(line) {
-    this.text = line;
-    var lineNode = this;
-    lineNode.tokens = [];
-    // Split the line into an array of clauses that include their terminal punctuation
-    line.match(/ ?([^,.:;?!)'"]+[,.:;?!)'"]+)/g).forEach(function(clauseWithPunctuation) {
-      var punctuation = clauseWithPunctuation.match(/[,.:;?!)'"]+/);
-      var clause = clauseWithPunctuation.slice(0, -punctuation.length);
-      var words = clause.split(' ');
-      // Loop over words of clause removing matches from front
-      while (words.length > 0) {
-        next_while_loop:
-        // for each loop, try each contracting prefix of the current word array
-        for (var i = words.length; i > 0; i--) {
-          var substring = words.slice(0, i).join(' ');
-          if (ics.hasOwnProperty(substring.toLowerCase())) {
-            //if there's a match, remove it from the words array, make a token, and continue to next while loop
-            lineNode.tokens.push(new LinkToken(substring));
-            words = words.slice(i);
-            break next_while_loop;
-          }
-          // if you get to the end of the words array with no match, remove and tokenize the first word
-          var word = words.shift();
-          lineNode.tokens.push(new TextToken(word));
+function LineNode(line, ics) {
+  this.text = line;
+  var lineNode = this;
+  lineNode.tokens = [];
+  // Split the line into an array of clauses that include their terminal punctuation
+  line.match(/ ?([^,.:;?!)'"]+[,.:;?!)'"]+)/g).forEach(function(clauseWithPunctuation) {
+    var punctuation = clauseWithPunctuation.match(/[,.:;?!)'"]+/);
+    var clause = clauseWithPunctuation.slice(0, -punctuation.length);
+    var words = clause.split(' ');
+    // Loop over words of clause removing matches from front
+    while (words.length > 0) {
+      next_while_loop:
+      // for each loop, try each contracting prefix of the current word array
+      for (var i = words.length; i > 0; i--) {
+        var substring = words.slice(0, i).join(' ');
+        if (ics.hasOwnProperty(substring.toLowerCase())) {
+          //if there's a match, remove it from the words array, make a token, and continue to next while loop
+          lineNode.tokens.push(new LinkToken(substring));
+          words = words.slice(i);
+          break next_while_loop;
         }
+        // if you get to the end of the words array with no match, remove and tokenize the first word
+        var word = words.shift();
+        lineNode.tokens.push(new TextToken(word));
+      }
+    }
+  });
+}
+
+function assembleTree(parentNode, icBlockNodes, ics) {
+  parentNode.lines.forEach(function(lineNode, lineIndex) {
+    // Take the link tokens of the block node. For each link node:
+    lineNode.tokens.filter(function(token){return token.constructor === LinkToken}).forEach(function(linkToken, tokenIndex) {
+      // if the link node is an ic, make its type ic
+      if (lineIndex === 0 && tokenIndex === 0) {
+        linkToken.type = 'ic';
+      } else if (icBlockNodes.hasOwnProperty(linkToken.ic)) {
+        // if there is still a block node under that ic, make it the exclusive child and delete its key
+        parentNode.children.push(icBlockNodes[linkToken.ic]);
+        delete icBlockNodes[linkToken.ic];
+        linkToken.type = 'primary'
+      } else {
+        // otherwise make it a secondary non-exclusive link
+        linkToken.type = 'secondary'
       }
     });
-  }
+  });
 
-  function assembleTree(parentNode, icBlockNodes) {
-    parentNode.lines.forEach(function(lineNode, lineIndex) {
-      // Take the link tokens of the block node. For each link node:
-      lineNode.tokens.filter(function(token){return token.constructor === LinkToken}).forEach(function(linkToken, tokenIndex) {
-        // if the link node is an ic, make its type ic
-        if (lineIndex === 0 && tokenIndex === 0) {
-          linkToken.type = 'ic';
-        } else if (icBlockNodes.hasOwnProperty(linkToken.ic)) {
-          // if there is still a block node under that ic, make it the exclusive child and delete its key
-          parentNode.children.push(icBlockNodes[linkToken.ic]);
-          delete icBlockNodes[linkToken.ic];
-          linkToken.type = 'primary'
-        } else {
-          // otherwise make it a secondary non-exclusive link
-          linkToken.type = 'secondary'
-        }
-      });
-    });
+  // Do the same for the children added in the last pass
+  parentNode.children.forEach(function(childNode) {
+    assembleTree(childNode, icBlockNodes);
+  });
 
-    // Do the same for the children added in the last pass
-    parentNode.children.forEach(function(childNode) {
-      assembleTree(childNode, icBlockNodes);
-    });
+  return parentNode;
+}
 
-    return parentNode;
-  }
+function TextToken(text) {
+  this.text = text;
+}
 
-  function TextToken(text) {
-    this.text = text;
-  }
+function LinkToken(text) {
+  this.text = text;
+  this.ic = text.toLowerCase();
+}
 
-  function LinkToken(text) {
-    this.text = text;
-    this.ic = text.toLowerCase();
-  }
-
-  function icOf(string) {
-    return string.split(/[,.;:?!]/)[0].toLowerCase();
-  }
+function icOf(string) {
+  return string.split(/[,.;:?!]/)[0].toLowerCase();
 }
--- test/spec/test.js
+++ test/spec/test.js
@@ -2,8 +2,23 @@
   'use strict';
 
   describe('Moss', function () {
-    it('should create an AST', function () {
+    it('should exist', function () {
       expect(!!Moss).toBe(true);
+    });
+    it('should build a single-node AST', function() {
+      var AST = Moss('Hello world.');
+      console.log(AST);
+      expect(AST.text).toBe('Hello world.');
+      expect(AST.constructor).toBe(BlockNode);
+      expect(AST.children).toEqual([]);
+
+      expect(AST.lines[0].constructor).toBe(LineNode);
+      expect(AST.lines[0].text).toBe('Hello world.');
+
+      expect(AST.lines[0].tokens[0].constructor).toBe(LinkToken);
+      expect(AST.lines[0].tokens[0].type).toBe('ic');
+      expect(AST.lines[0].tokens[0].text).toBe('Hello world');
+      expect(AST.lines[0].tokens[0].ic).toBe('hello world');
     });
   });
 })();
