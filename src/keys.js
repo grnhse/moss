0c1247f30421fbfd33e62fabfa2b58c9ec8e24f4
Allen Krulwich <akrulwich@greenhouse.io>
2016-07-21 09:09:03 -0400

    Command tab opens alias in new window

--- src/keys.js
+++ src/keys.js
@@ -1,37 +1,40 @@
 document.onkeydown = function(e) {
-  if (e.metaKey || e.altKey || e.shiftKey || e.ctrlKey) {
-    return;
-  }
 
   // The current link is the lowest selected link
   var selectedLinks = document.getElementsByClassName('selected-link')
   var currentLink = selectedLinks[selectedLinks.length - 1];
   var rootElement = document.getElementById('_moss').firstChild;
 
-  if (e.keyCode === 37 || e.keyCode === 72) { // Left
-    if (currentLink.parentNode.parentNode === rootElement) { // If we are at the root, do nothing
-      window.location.hash = rootElement.firstChild.firstChild.id
-    } else {
-      window.location.hash = currentLink.parentNode.firstChild.id.slice(0, -1);
-    }
-  } else if (e.keyCode === 39 || e.keyCode === 76) { // Right
-    // If a primary link is selected, bold the ic of the child paragraph
-    if (currentLink.dataset.type === 'parent') {
-      window.location.hash = currentLink.id + '/';
-    }
-  } else if (e.keyCode === 38 || e.keyCode === 75) { // Up
-    var links = currentLink.parentNode.querySelectorAll('a');
-    if (currentLink !== links[0]){
-      window.location.hash = links[Array.prototype.slice.call(links).indexOf(currentLink) - 1].id;
-    }
-  } else if (e.keyCode === 40 || e.keyCode === 74) { // Down
-    var links = currentLink.parentNode.querySelectorAll('a');
-    if (currentLink !== links[links.length - 1]){
-      window.location.hash = links[Array.prototype.slice.call(links).indexOf(currentLink) + 1].id;
+  if (!(e.metaKey || e.altKey || e.shiftKey || e.ctrlKey)) {
+    if (e.keyCode === 37 || e.keyCode === 72) { // Left
+      if (currentLink.parentNode.parentNode === rootElement) { // If we are at the root, do nothing
+        window.location.hash = rootElement.firstChild.firstChild.id
+      } else {
+        window.location.hash = currentLink.parentNode.firstChild.id.slice(0, -1);
+      }
+    } else if (e.keyCode === 39 || e.keyCode === 76) { // Right
+      // If a primary link is selected, bold the ic of the child paragraph
+      if (currentLink.dataset.type === 'parent') {
+        window.location.hash = currentLink.id + '/';
+      }
+    } else if (e.keyCode === 38 || e.keyCode === 75) { // Up
+      var links = currentLink.parentNode.querySelectorAll('a');
+      if (currentLink !== links[0]){
+        window.location.hash = links[Array.prototype.slice.call(links).indexOf(currentLink) - 1].id;
+      }
+    } else if (e.keyCode === 40 || e.keyCode === 74) { // Down
+      var links = currentLink.parentNode.querySelectorAll('a');
+      if (currentLink !== links[links.length - 1]){
+        window.location.hash = links[Array.prototype.slice.call(links).indexOf(currentLink) + 1].id;
+      }
     }
   } else if (e.keyCode === 13) {
     if (currentLink.dataset.type === 'alias') {
-      window.location.hash = currentLink.dataset.targetId;
+      if (e.metaKey || e.ctrlKey) {
+        window.open(window.location.href.split('#')[0] + '#' + currentLink.dataset.targetId, '_blank');
+      } else {
+        window.location.hash = currentLink.dataset.targetId;
+      }
     } else if (currentLink.dataset.type === 'parent') {
       window.location.hash = currentLink.id + '/';
     }
